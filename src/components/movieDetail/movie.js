3ba764dc697ab39403ef68362ef5ef4c5619c1eb
mrppradana <ridhoputra34@gmail.com>
2023-09-27 09:35:37 +0700

    testing build 2

--- src/components/movieDetail/movie.js
+++ src/components/movieDetail/movie.js
@@ -9,10 +9,10 @@ const Movie = () => {
     useEffect(() => {
         getData()
         window.scrollTo(0,0)
-    }, )
+    }, [])
 
     const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=51c9d5760571e85e84cae213864ebd12&language=en-US`)
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
         .then(res => res.json())
         .then(data => setMovie(data))
     }
@@ -20,12 +20,12 @@ const Movie = () => {
     return (
         <div className="movie">
             <div className="movie__intro">
-                <img className="movie__backdrop" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.backdrop_path : ""}`} alt="img" />
+                <img className="movie__backdrop" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.backdrop_path : ""}`} />
             </div>
             <div className="movie__detail">
                 <div className="movie__detailLeft">
                     <div className="movie__posterBox">
-                        <img className="movie__poster" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.poster_path : ""}`} alt="img" />
+                        <img className="movie__poster" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.poster_path : ""}`} />
                     </div>
                 </div>
                 <div className="movie__detailRight">
@@ -58,14 +58,36 @@ const Movie = () => {
                 </div>
             </div>
             <div className="movie__links">
-                <div className="movie__heading">Useful Links</div>
-                {
-                    currentMovieDetail && currentMovieDetail.homepage && <a href={currentMovieDetail.homepage} target="_blank" style={{textDecoration: "none"}}><p><span className="movie__homeButton movie__Button">Homepage <i className="newTab fas fa-external-link-alt"></i></span></p></a>
-                }
-                {
-                    currentMovieDetail && currentMovieDetail.imdb_id && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} target="_blank" style={{textDecoration: "none"}}><p><span className="movie__imdbButton movie__Button">IMDb<i className="newTab fas fa-external-link-alt"></i></span></p></a>
-                }
-            </div>
+        <div className="movie__heading">Useful Links</div>
+        {currentMovieDetail && currentMovieDetail.homepage && (
+          <a
+            href={currentMovieDetail.homepage}
+            target="_blank"
+            rel="noreferrer" // Add rel="noreferrer" for security
+            style={{ textDecoration: "none" }}
+          >
+            <p>
+              <span className="movie__homeButton movie__Button">
+                Homepage <i className="newTab fas fa-external-link-alt"></i>
+              </span>
+            </p>
+          </a>
+        )}
+        {currentMovieDetail && currentMovieDetail.imdb_id && (
+          <a
+            href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id}
+            target="_blank"
+            rel="noreferrer" // Add rel="noreferrer" for security
+            style={{ textDecoration: "none" }}
+          >
+            <p>
+              <span className="movie__imdbButton movie__Button">
+                IMDb<i className="newTab fas fa-external-link-alt"></i>
+              </span>
+            </p>
+          </a>
+        )}
+      </div>
             <div className="movie__heading">Production companies</div>
             <div className="movie__production">
                 {
